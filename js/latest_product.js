2446606a5f78d4b33b4a2cda59446d3fb71d5a52
hirensinh84 <hirengohil1686@gmail.com>
2025-12-16 18:44:08 +0530

    third commit

---
+++ js/latest_product.js
@@ -0,0 +1,10 @@
+export const letestp = [
+    { id: 8, price: 3200, name: "Office chair", img: "productimage/oimg5.jpg", totalstocks: 50, category:"office" },
+     { id: 3, price: 12000, name: "Office chai", img: "productimage/oimg2.jpg", totalstocks: 40, category:"office" },
+     { id: 5, price: 2200, name: "Student chair", img: "productimage/stimg1.jpg", totalstocks: 40, category:"student" },
+     { id: 18, price: 8500, name: "Cafe chair", img: "productimage/cfimg2.jpg", totalstocks: 10,category:"cafe" },
+    { id: 16, price: 9000, name: "Gaming chair", img: "productimage/gimg4.jpg", totalstocks: 16,category:"gaming" },
+    { id: 22, price: 3000, name: "Student chair", img: "productimage/stimg7.jpg", totalstocks: 10,category:"student" },
+   
+   
+];
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+import { letestp } from './latest_product.js';
+
+
 const slider = document.getElementById("slider");
 const image = slider.querySelectorAll("img");
+
 
 let count = 0;
 
@@ -19,15 +23,6 @@ function showSlide(i) {
     slider.style.transform = `translateX(${-count * 100}vw)`;
 }
 
-// const backbutton = document.querySelector(".back")
-// backbutton.addEventListener("click", () => {
-//     showSlide(count - 1);
-// });
-
-// const nextbutton = document.querySelector(".next")
-// nextbutton.addEventListener("click", () => {
-//     showSlide(count + 1);
-// });
 
 
 setInterval(function(){
@@ -43,23 +38,15 @@ setInterval(function(){
 let plist = document.getElementById("product-list")
 
 
-let products = [
-    { id: 1, name: "Orang", price: 29.99, image: "ch1.png" },
-    { id: 2, name: "Apple", price: 19.99, image: "ch2.png" },
-    { id: 3, name: "Mango", price: 9.99, image: "ch3.png" },
-    { id: 4, name: "Grapes", price: 24.99, image: "ch4.png" },
-    { id: 4, name: "Banana", price: 13.99, image: "ch5.png" },
-]
 
-products.forEach(products => {
+letestp.forEach(products => {
     let item = document.createElement("div")
     item.className = "Product"
     item.innerHTML = `
-       <img src="${products.image}" alt="${products.name}">
+       <img src="${products.img}" alt="${products.name}">
        <h3>${products.name}</h3>
-       <p>Price: $${products.price}</p>
-       <button>Add to Cart</button>
+       
        `
-    plist.appendChild(item)
+    plist.append(item)
 
 })
